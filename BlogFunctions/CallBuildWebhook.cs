0a331735167f647e448e79fe43ff63056bed0e38
Andrew Lock <andrewlock.net@gmail.com>
2018-09-27 19:45:47 +0100

    Add BuildHook URL timer trigger

---
+++ BlogFunctions/CallBuildWebhook.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace BlogFunctions
+{
+    public static class CallBuildWebhook
+    {
+        [FunctionName("CallBuildWebhook")]
+        public static async Task RunAsync([TimerTrigger("0 15 10 * * 2")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"Tiggering Blog rebuild at: {DateTime.Now}");
+
+            await TriggerBuild();
+
+            log.LogInformation($"Blog rebuild triggered successfully at: {DateTime.Now}");
+        }
+
+        static async Task TriggerBuild()
+        {
+            var BuildHookBaseUrl = Environment.GetEnvironmentVariable("BuildHookBaseUrl");
+            var path = Environment.GetEnvironmentVariable("BuildHookPath");
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("BuildHookBaseUrl");
+
+                await client.PostAsJsonAsync(path, "{}");
+            }
+        }
+    }
+}
